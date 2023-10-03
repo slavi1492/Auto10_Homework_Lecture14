fb544a8197e8580833d6472818aecfcf12b607e9
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-10-03 18:39:56 +0300

    Changes

--- src/test/java/LoginScenariosTests.java
+++ src/test/java/LoginScenariosTests.java
@@ -1,12 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -22,6 +22,14 @@ public class LoginScenariosTests {
     final String LOGIN_URL = BASE_URL + "/users/login";
     final String USER_URL = BASE_URL + "/users/4068";
 
+    @DataProvider(name = "validUser")
+    public Object[][] getUser() {
+        return new Object[][]{
+                {"auto_user", "auto_pass"}
+        };
+    }
+
+
     @BeforeMethod
     public void driverSetup() {
         WebDriverManager.chromedriver().setup();
@@ -29,25 +37,42 @@ public class LoginScenariosTests {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait = new FluentWait<>(driver);
         wait.ignoring(NoSuchElementException.class);
         wait.pollingEvery(Duration.ofMillis(250));
         wait.withTimeout(Duration.ofSeconds(5));
     }
 
 
-    @Test
-    @Parameters ({"username", "password"})
-    public void positiveLoginTest (String username, String password){
+    @Test(dataProvider = "validUser")
+    // @Parameters ({"username", "password"})
+    // public void positiveLoginTest(String username, String password) {
+    public void positiveLoginTest(String username, String password) {
 
-        System.out.println(username + password);
-       // driver.get(BASE_URL);
+        System.out.println("Open home page");
+        driver.get(BASE_URL);
+
+        System.out.println("Click 'Login' button");
+        WebElement loginButton = driver.findElement(By.id("nav-link-login"));
+//        wait.until(ExpectedConditions.elementToBeClickable(loginButton));
+//        loginButton.click();
+        clickButton(loginButton);
+
+        System.out.println("Verify that you reach login page");
+
+
 
     }
 
     @AfterMethod
-    public void closeDriver(){
+    public void closeDriver() {
         driver.close();
+    }
+
+    public void clickButton(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+
     }
 
 }
