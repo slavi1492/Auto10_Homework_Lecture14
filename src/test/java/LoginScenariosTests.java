cfda4782541312ae9c8e2e6e608ea98e40e288e6
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-10-05 17:15:37 +0300

    Changes

--- src/test/java/LoginScenariosTests.java
+++ src/test/java/LoginScenariosTests.java
@@ -35,10 +35,10 @@ public class LoginScenariosTests {
     @DataProvider(name = "invalidUser")
     public Object[][] getInvalidUser() {
         return new Object[][]{
-                {"wrong_user", "auto_pass"},
-                {"auto_user", "wrong_pass"},
-                {"", "auto_pass"},
-                {"auto_user", ""}
+                {"wrong_user", "auto_pass", "User not found"},
+                {"auto_user", "wrong_pass", "Ivalid password"},
+                {"", "auto_pass", "UsernameOrEmail cannot be empty"},
+                {"auto_user", "", "Password cannot be empty"}
         };
     }
 
@@ -55,7 +55,7 @@ public class LoginScenariosTests {
     }
 
 
-    @Test(dataProvider = "validUser")
+    @Test(groups = "PositiveTest", dataProvider = "validUser")
     // @Parameters ({"username", "password"})
     // public void positiveLoginTest(String username, String password) {
     public void positiveLoginTest(String username, String password) {
@@ -110,14 +110,53 @@ public class LoginScenariosTests {
 
         System.out.println("Click 'Sign out' button");
         WebElement signOutButton = driver.findElement(By.cssSelector(".fa-sign-out-alt"));
-        wait.until(ExpectedConditions.elementToBeClickable(signOutButton));
-        signOutButton.click();
+        clickButton(signOutButton);
 
         System.out.println("Verify that we are signed out of profile");
         wait.until(ExpectedConditions.visibilityOf(signInOutMessageBox));
         signInOutMessageBoxText = signInOutMessageBox.getText();
         Assert.assertEquals(signInOutMessageBoxText.trim(), "Successful logout!");
         wait.until(ExpectedConditions.invisibilityOf(navBarProfileButton));
+    }
+
+    @Test(groups = "negativeTests", dataProvider = "invalidUser")
+    public void negativeLoginTest(String invalidUsername, String invalidPassword, String errorMessage) {
+
+        System.out.println("Open home page");
+        driver.get(BASE_URL);
+
+        System.out.println("Click 'Login' button");
+        WebElement loginButton = driver.findElement(By.id("nav-link-login"));
+        clickButton(loginButton);
+
+        System.out.println("Verify that you reach login page");
+        wait.until(ExpectedConditions.urlToBe(LOGIN_URL));
+
+        System.out.println("Find 'Sign In' form");
+        WebElement signInForm = driver.findElement(By.cssSelector("form"));
+        wait.until(ExpectedConditions.visibilityOf(signInForm));
+
+        System.out.println("Populate 'Username' input");
+        WebElement usernameInputField = signInForm.findElement(By.id("defaultLoginFormUsername"));
+        wait.until(ExpectedConditions.visibilityOf(usernameInputField));
+        usernameInputField.sendKeys(invalidUsername);
+
+        System.out.println("Populate 'Password' input");
+        WebElement passwordInputField = signInForm.findElement(By.id("defaultLoginFormPassword"));
+        wait.until(ExpectedConditions.visibilityOf(passwordInputField));
+        passwordInputField.sendKeys(invalidPassword);
+
+        System.out.println("Click 'Sign In' button");
+        WebElement signInButton = signInForm.findElement(By.id("sign-in-button"));
+        clickButton(signInButton);
+
+        WebElement signInOutMessageBox = driver.findElement(By.id("toast-container"));
+        wait.until(ExpectedConditions.visibilityOf(signInOutMessageBox));
+        String signInOutMessageBoxText = signInOutMessageBox.getText();
+        Assert.assertEquals(signInOutMessageBoxText, errorMessage);
+        Assert.assertEquals(driver.getCurrentUrl(), LOGIN_URL);
+
+
     }
 
     @AfterMethod
